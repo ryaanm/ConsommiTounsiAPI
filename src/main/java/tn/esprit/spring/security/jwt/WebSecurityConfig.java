5916a6bd813612feac59c0f6d3b91fb963cfc1d0
Dahmeni <karim_dahmeni@hotmail.com>
2022-05-17 01:43:16 +0100

    add docker

--- src/main/java/tn/esprit/spring/security/jwt/WebSecurityConfig.java
+++ src/main/java/tn/esprit/spring/security/jwt/WebSecurityConfig.java
@@ -55,11 +55,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable()
-			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
-			.antMatchers("/api/test/**").permitAll()
-			.anyRequest().authenticated();
+			.authorizeRequests().antMatchers("/").permitAll();
+
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
